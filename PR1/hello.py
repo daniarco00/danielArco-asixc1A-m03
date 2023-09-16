dd046a222c7704762d882a504a85d24b37e9f1c2
Daniel <daniel.arco.7e5@itb.cat>
2023-09-16 18:14:37 +0200

    commit

---
+++ PR1/hello.py
@@ -0,0 +1,14 @@
+
+#This is a commnent
+print("Hello world :) ")
+
+
+"""
+Danil Arco Guasch
+16/09/2023
+ASIXc A M03 UD1 A1
+Descripció: 
+Estructura d'un programa informàtic -  Algoritmes - Git
+...
+...
+"""
