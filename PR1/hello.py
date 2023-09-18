b24f93e38524d989b724f0d139336b5eb7d9af4e
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2023-09-18 12:28:04 +0200

    FET

--- PR1/hello.py
+++ PR1/hello.py
@@ -2,13 +2,16 @@
 #This is a commnent
 print("Hello world :) ")
 
-
 """
 Danil Arco Guasch
-16/09/2023
-ASIXc A M03 UD1 A1
+18/09/2023
+ASIXc A M03 UF1 A1
 Descripció: 
 Estructura d'un programa informàtic -  Algoritmes - Git
 ...
 ...
 """
+
+
+gfgffgf
+
