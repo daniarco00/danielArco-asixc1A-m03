9967e4de97060f94ecfa13f8b1e82705140679e1
Daniel <daniel.arco.7e5@itb.cat>
2023-09-20 18:06:16 +0200

    commit

---
+++ UF1/hello.py
@@ -0,0 +1,15 @@
+
+#This is a commnent
+print("Hello world :) ")
+
+"""
+Danil Arco Guasch
+18/09/2023
+ASIXc A M03 UF1 A1
+Descripció: 
+Estructura d'un programa informàtic -  Algoritmes - Git
+...
+...
+"""
+
+
