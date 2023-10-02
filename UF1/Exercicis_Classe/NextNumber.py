3cb5cd284c5b83c9abd5d265636cbbc8abfa19fb
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2023-10-02 13:04:22 +0200

    COMMIT

---
+++ UF1/Exercicis_Classe/NextNumber.py
@@ -0,0 +1,14 @@
+"""
+Daniel Arco
+ASIXc M03-UF1 A2 MANIPULACIÓ DE DADES
+Et demana un numero i despres ve el seguent...
+"""
+valor = int(input("Introdueix numero: "))
+
+valor_seguent = valor + 1
+print(" Despres va el", valor_seguent)
+
+
+
+
+
