efac3d9b8c473639e72c00575720496b89de17fe
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2023-12-18 12:54:25 +0100

    COMMIT

---
+++ A4/exercicis_avançats/canadianTree.py
@@ -0,0 +1,17 @@
+"""
+Fer un programa que dibuixa un avet del Canadà.
+Cal demanar la mida de l'arbre. La mida inclou el tronc i les branques
+"""
+
+brick = "🌿"
+n = int(input())
+
+for i in range(1, n+1):
+    if i >= 0.8 * n:
+        espais_tronc = " " * (int((0.8 * n)))
+        amplada_tronc = "🏾" * int((0.2 * n))
+        print(f"{espais_tronc}{amplada_tronc}")
+    else:
+        espacio = " " * (n - i)
+        bricks = brick * i
+        print(espacio + bricks)
