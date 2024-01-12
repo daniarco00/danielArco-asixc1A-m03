e746867e557b596a185561898e003bb38b283a62
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2024-01-12 12:59:53 +0100

    COMMIT

--- A5/dayofWeek.py
+++ A5/dayofWeek.py
@@ -1,15 +1,20 @@
 
 try:
     dies_setmana = ["Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge"]
+    dias_setmana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+    days_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Fryday", "Saturday", "Sunday"]
+
     numero = int(input(""))
 
-    if numero > 7:
+    if numero > 7 or numero < 1:
         raise TypeError
 
-    for num in range(0,8):
+    for num in range(1,8):
         if num == numero:
             num -= 1
             print(dies_setmana[num])
+            print(dias_setmana[num])
+            print(days_week[num])
 
 except:
     print("Escriu un numero del 1-7")
