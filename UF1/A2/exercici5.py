e8df46b252dcd3453bb8136d9f2368972e01f0b5
Daniel <daniel.arco.7e5@itb.cat>
2024-02-01 20:11:02 +0100

    commit

--- UF1/A2/exercici5.py
+++ UF1/A2/exercici5.py
@@ -1,6 +1,6 @@
 print("Benvolgut al convertidor de graus Fahrenheis a graus Celsius.")
 
-F = float(input("Introdueix la temperatura que dessitges convertir: "))
+F = float(input("Introdueix la TEMPERATURA que dessitges convertir: "))
 
 
 C = (F - 32) * 5/9
