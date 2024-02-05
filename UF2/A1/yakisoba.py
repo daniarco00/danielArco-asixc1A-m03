b1cccf9cef97990834e0bf2e29dc4dc711bd7213
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2024-02-05 12:10:50 +0100

    COMMIT

---
+++ UF2/A1/yakisoba.py
@@ -0,0 +1,51 @@
+"""
+Danieh
+"""
+
+from time import sleep
+# region Declaració de Funcions ----------
+def recopilar_ingredients():
+   print("Comprar al supermercat")
+   sleep(1)
+   print("Disposar-los sobre el marbre")
+   sleep(1)
+
+def cuinar_tallarines():
+    print("Preparar aigua.")
+    sleep(1)
+    print("Bullir tallarines.")
+    sleep(1)
+    print("Escorrer tallarines.")
+    sleep(1)
+    print("Deixar les preparades.")
+    sleep(1)
+
+def cuinar_pastanagues():
+    print("Tallar pastanagues.")
+    sleep(1)
+    print("Frgir pastanagues.")
+    sleep(1)
+    print("Deixar pastanagues preparades.")
+    sleep(1)
+
+def cuinar_cebes():
+    print("Tallar cebes.")
+    sleep(1)
+    print("Fregir pastanagues")
+    sleep(1)
+    print("Deixar cebes preparades")
+    sleep(1)
+
+def preparacio_final():
+    print("Barrejar ingredients amb salsa yakitori.")
+    sleep(1)
+    print("Deixar llest per servir.")
+
+# endregion
+# region INICI: Preparar fideus Yakisoba
+recopilar_ingredients()
+cuinar_tallarines()
+cuinar_pastanagues()
+cuinar_cebes()
+preparacio_final()
+# endregion
