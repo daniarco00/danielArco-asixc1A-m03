9c5d7796a5babecf85c76bf9ab0319d94e6587b6
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2024-02-16 13:03:55 +0100

    COMMIT

---
+++ UF2/A1/matchAnalyzer.py
@@ -0,0 +1,27 @@
+"""
+Daniel
+"""
+
+# region Obtenir equips
+def obtenir_equips():
+        equipoA = input()
+        equipoB = input()
+        print(f"{equipoA}, {equipoB}")
+#endregion
+
+# region Obtenir puntuacions
+def obtenir_puntuacions():
+    n = 0
+    while n != -1:
+        valorX = [int(i) for i in input().split()]
+
+#endregion
+
+
+
+
+llista = []
+
+
+
+obtenir_equips()
