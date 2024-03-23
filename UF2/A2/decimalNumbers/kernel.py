d18a366f0f6dd661591ab11fb07a24b12c51c487
Daniel <daniel.arco.7e5@itb.cat>
2024-03-23 20:11:02 +0100

    commit

--- UF2/A2/decimalNumbers/kernel.py
+++ UF2/A2/decimalNumbers/kernel.py
@@ -1,17 +1,21 @@
 import numeros as n
-
+#region Recoger numeros
 def introducirNumeros(): # Función que pide al usuario que introduzca un numeros en una lista
     numeros = []
-    entrada = input()
-    for num in entrada:
-        numeros.append(num)
+    while True:
+        entrada = input()
+        if entrada == ':Q':
+            break
+        for palabra in entrada.split():
+            numeros.append(palabra)
     return numeros
-
+#endregion
+#region Imprimir numeros
 def imprimir_numeros(numeros):
-    for i in numeros:
-        if i in n.NUMEROS:
-            print(n.NUMEROS[i])
-
-# Ejemplo de uso
-numeros = introducirNumeros()
-imprimir_numeros(numeros)
+    output = ""
+    for j in range(len(n.NUMEROS[int(numeros[0])])):
+        for k in range(0, len(numeros)):
+            output += n.NUMEROS[int(numeros[k])][j] + "  "
+        output += "\n"
+    print(output)
+#endregion
